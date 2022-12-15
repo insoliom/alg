f662f85f19d4e15bfa20d41708eec2d871b8640d
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2022-12-15 17:30:45 +0200

    Third commit

---
+++ zaglushki.cs
@@ -0,0 +1,14 @@
+﻿
+public class Program
+{
+    public static void Main(string[]args)
+    {
+        int rows,cols;
+        rows=cols=0
+        int[,] array;
+        void MatrixTraversal(int[,] array)
+        {
+
+        }
+
+    }
